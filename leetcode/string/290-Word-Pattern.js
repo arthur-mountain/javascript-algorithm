5495e3bf2c5100502e2d245933fb3d8c0c6c29e9
arthur <arthur.oceanus@gmail.com>
2023-01-21 15:02:26 +0800

    add leetcode 290

--- leetcode/string/290-Word-Pattern.js
+++ leetcode/string/290-Word-Pattern.js
@@ -1,7 +1,27 @@
-const main = (s, t) => {
+const main = (pattern, str) => {
+  const pAry = pattern.split("");
+  const sAry = str.split(/\s/);
 
+  if (pAry.length !== sAry.length) return false;
+
+  const map = new Map();
+
+  for (const idx in pAry) {
+    const p = pAry[idx];
+    const s = sAry[idx];
+    const v = map.get(p);
+
+    if (!v) {
+      map.set(p, s);
+    } else if (v !== s) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
-console.log("1. Word Pattern: ", main("egg", "add"));
-console.log("2. Word Pattern: ", main("foo", "bar"));
-console.log("3. Word Pattern: ", main("paper", "title"));
+console.log("1. Word Pattern: ", main("abba", "dog cat cat dog"));
+console.log("2. Word Pattern: ", main("abba", "dog cat cat fish"));
+console.log("3. Word Pattern: ", main("aaaa", "dog cat cat dog"));
+console.log("4. Word Pattern: ", main("aaaa", "dog dog dog dog"));
