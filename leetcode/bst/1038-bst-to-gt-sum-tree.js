c26076db2a9100a6435b2f99af8c5be8172d10ca
arthur <arthur.oceanus@gmail.com>
2024-06-26 01:22:53 +0800

    wip

--- leetcode/bst/1038-bst-to-gt-sum-tree.js
+++ leetcode/bst/1038-bst-to-gt-sum-tree.js
@@ -15,10 +15,12 @@ const bstToGst = (root) => {
 
   // - Go to rightmost node
   //  - leaf node have same value
-  //  - go up to parent node
-  //    - parnet node value = parent node value + first right child value
   //
-  //    - parent node left child value = parent node left child value + parent node value
+  //  - go up to parent node
+  //    - parnet node value += first right child value
+  //
+  //    - parent node left child value +=  parent node value + sum of parent node left child right child value
+  //      - sum of parent node left child right child value
   //
   //  - until go up to root node
   //    - Go to rightmost node of left child node of root node
@@ -27,6 +29,44 @@ const bstToGst = (root) => {
   //    - go up to parent node
   //      - parnet node value = parent node value + first right child value
   //      - parent node left child value = parent node left child value + parent node value
+
+  const getDfsRightStack = (root) => {
+    let current = root;
+    let stack = [];
+    // 1. Go to rightmost node
+    while (current) {
+      stack.push(current);
+      current = current.right;
+    }
+    return stack;
+  };
+
+  const calculate = (stack) => {
+    while (stack.length) {
+      // 2. Go up to parent node
+      let node = stack.pop();
+
+      // 2.1 parnet node value += first right child value
+      if (node.right) node.val += node.right.val;
+
+      if (node.left) {
+        if (node.left.right) calculate(getDfsRightStack(node.left));
+        node.left.val += node.val + (node.left.right ? node.left.right.val : 0);
+      }
+    }
+  };
+
+  calculate(getDfsRightStack(root));
+  // 4. until go up to root node
+  const stack = getDfsRightStack(root.left);
+  while (stack.length) {
+    let node = stack.pop();
+
+    node.val += root.val + (node.right ? node.right.val : 0);
+
+    if (node.left) {
+    }
+  }
 
   // const _bstToGst = (root) => {
   //   // find the rightmost node
@@ -58,5 +98,5 @@ const bstToGst = (root) => {
   // };
   //
   // _bstToGst(root);
-  // return root;
+  return root;
 };
