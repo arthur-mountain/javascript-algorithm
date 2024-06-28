5c2756917e4eab9899f74a2a7fc317c9135b5860
arthur <arthur.oceanus@gmail.com>
2024-06-28 20:41:15 +0800

    2285 first commit

---
+++ leetcode/graph/2285.maximum-total-importance-of-roads.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} n
+ * @param {number[][]} roads
+ * @return {number}
+ */
+var maximumImportance = function (n, roads) {
+  // the node that connected to the most other nodes should be n,
+  // the second most connected node should be n-1, and so on
+};
+
+console.log(
+  "first sum: ",
+  maximumImportance([
+    [0, 1],
+    [1, 2],
+    [2, 3],
+    [0, 2],
+    [1, 3],
+    [2, 4],
+  ]),
+);
+console.log(
+  "first sum: ",
+  maximumImportance([
+    [0, 3],
+    [2, 4],
+    [1, 3],
+  ]),
+);
