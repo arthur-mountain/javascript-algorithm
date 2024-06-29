153d7affd4961c05863078528f10bd8aa50affe9
arthur <arthur.oceanus@gmail.com>
2024-06-30 01:20:49 +0800

    improve 1791

--- leetcode/graph/1791.find-center-of-star-graph.js
+++ leetcode/graph/1791.find-center-of-star-graph.js
@@ -1,7 +1,9 @@
 /*
  * - [X] Done
  *   - iterate all edges until the vertex count equals to the length of edges
- * - [] Refer to what others are doing
+ * - [X] Refer to what others are doing
+ *   - there must have a center vertex in star graph,
+ *   - so we only need to check the first and second edge
  */
 /**
  * @param {number[][]} edges
@@ -16,5 +18,18 @@ var findCenter = function (edges) {
     if (map[u] === length) return u;
     map[v] = (map[v] || 0) + 1;
     if (map[v] === length) return v;
+  }
+};
+
+var findCenterReferredSolution = function (edges) {
+  // this star graph have to a center vertex,
+  // each edge must be connected to center vertex, so
+  // we only check the first and second edge.
+
+  // check the first vertex in first edge is as same as second edge first or second vertex
+  if (edges[0][0] === edges[1][0] || edges[0][0] === edges[1][1]) {
+    return edges[0][0];
+  } else {
+    return edges[0][1];
   }
 };
