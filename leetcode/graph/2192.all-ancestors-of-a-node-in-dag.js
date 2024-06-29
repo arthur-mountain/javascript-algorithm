abbcc533e66e0f840277993139e4a75f44cf8b3e
arthur <arthur.oceanus@gmail.com>
2024-06-30 02:18:08 +0800

    wip

--- leetcode/graph/2192.all-ancestors-of-a-node-in-dag.js
+++ leetcode/graph/2192.all-ancestors-of-a-node-in-dag.js
@@ -12,15 +12,14 @@ var getAncestors = function (n, edges) {
     return [];
   }
 
-  const result = Array.from({ length: n - 1 }, () => []);
-  console.log({ result });
+  const result = Array.from({ length: n }, () => []);
+  console.log(result);
 
   // each to vertex(index is 1) has from(index is 0) as ancestor
   // but the ancestors should be sorted
-  for (let i = 0, lastIndex = edges.length; i < lastIndex; i++) {
+  for (let i = 0, length = edges.length; i < length; i++) {
     const [a, v] = edges[i];
 
-    // failed in here, could not found result[v]
     result[v].push(a);
 
     if (result[a].length) {
@@ -28,26 +27,31 @@ var getAncestors = function (n, edges) {
     }
   }
 
+  for (let i = 0, length = result.length; i < length; i++) {
+    result[i] = [...new Set(result[i])].sort();
+  }
+
   return result;
 };
 try {
   console.log(
-    getAncestors(8, [
+    getAncestors(6, [
       [0, 3],
-      [0, 4],
+      [5, 0],
+      [2, 3],
+      [4, 3],
+      [5, 3],
       [1, 3],
-      [2, 4],
-      [2, 7],
-      [3, 5],
-      [3, 6],
-      [3, 7],
-      [4, 6],
+      // TODO: add 2 as ancestor of 5 after [5,0] so the 0 index will less 2
+      [2, 5],
+      [0, 1],
+      [4, 5],
+      [4, 2],
+      [4, 0],
+      [2, 1],
+      [5, 1],
     ]),
   );
-  //
-  // console.log(
-  // getAncestors(5, [[0,1],[0,2],[0,3],[0,4],[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]),
-  // );
 } catch (error) {
   console.error(error);
 }
