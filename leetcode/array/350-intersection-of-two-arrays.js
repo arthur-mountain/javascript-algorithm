05f9963b9bb11183f548973c5f7d15f212ff3782
arthur <arthur.oceanus@gmail.com>
2024-07-03 13:59:44 +0800

    350 first commit

---
+++ leetcode/array/350-intersection-of-two-arrays.js
@@ -0,0 +1,31 @@
+/*
+ * - [x] Done
+ *   - using mapping for long array and loop through short array
+ * - [] Refer to what others are doing
+ */
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+const intersect = (nums1, nums2) => {
+  const [long, short] =
+    nums1.length > nums2.length ? [nums1, nums2] : [nums2, nums1];
+  const longMap = new Map();
+
+  for (const num of long) {
+    longMap.set(num, (longMap.get(num) || 0) + 1);
+  }
+
+  let result = [];
+  for (const num of short) {
+    if (longMap.get(num)) {
+      result.push(num);
+      longMap.set(num, (longMap.get(num) || 0) - 1);
+    }
+  }
+
+  return result;
+};
+
+console.log(intersect([1, 2, 2, 1], [2, 2]));
