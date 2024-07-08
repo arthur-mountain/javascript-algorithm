617ebc59584d13c803c23820e7f3631df923ccd5
arthur <arthur.oceanus@gmail.com>
2024-07-08 14:22:04 +0800

    [wip] 1823

---
+++ leetcode/link-list/1823.js
@@ -0,0 +1,70 @@
+/*
+ * - [] Done
+ * - [] Refer to what others are doing
+ */
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+let findTheWinner = (n, k) => {
+  const circular = new CircularLinkedList(n);
+
+  let count = 0;
+  let current = circular.head;
+  while (circular.size !== 1) {
+    while (k > 1) {
+      current = current.next;
+      k--;
+    }
+    circular.delete(current);
+  }
+
+  circular.inspect();
+};
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class CircularLinkedList {
+  constructor(size) {
+    let head = new Node(1);
+    let tail = head;
+    let num = 2;
+    while (num <= size) {
+      const newNode = new Node(num);
+      tail.next = newNode;
+      tail = newNode;
+      num++;
+    }
+
+    this.head = head;
+    tail.next = this.head;
+    this.size = size;
+  }
+
+  delete(current) {
+    current.next = current.next.next;
+    this.size--;
+  }
+
+  inspect() {
+    console.log(require("node:util").inspect(this, { depth: null }));
+  }
+
+  print() {
+    let current = this.head;
+    do {
+      console.log(current.value);
+      current = current.next;
+    } while (current !== this.head);
+  }
+}
+
+findTheWinner(5, 2);
+
+// findTheWinner(6, 5);
