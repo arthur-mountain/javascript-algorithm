8b0d8fb624c8106d89f47ff63e64f1472a314959
arthur <arthur.oceanus@gmail.com>
2024-07-11 13:56:21 +0800

    [done] 1190 first commit

---
+++ leetcode/1190.js
@@ -0,0 +1,37 @@
+/*
+ * - [x] Done
+ *   - the inside string of parentheses will be reversed first
+ *   - and then the outside string of parentheses will be reversed
+ *   - finally, return the reversed string
+ * - [] Refer to what others are doing
+ */
+/**
+ * @param {string} s
+ * @return {string}
+ */
+let reverseParentheses = (s) => {
+  const helper = (s) => {
+    let str = "";
+    let char = "";
+
+    while (s.length) {
+      char = s.shift();
+
+      if (char === "(") {
+        str += helper(s);
+      } else if (char === ")") {
+        return str.split("").reverse().join("");
+      } else {
+        str += char;
+      }
+    }
+
+    return str;
+  };
+
+  return helper(s.split(""));
+};
+
+reverseParentheses("(abcd)");
+reverseParentheses("(u(love)i)");
+reverseParentheses("(ed(et(oc))el)");
