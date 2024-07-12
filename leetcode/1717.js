bb8b13b1e80fa0af578d56ab214f9fa01ef7a027
arthur <arthur.oceanus@gmail.com>
2024-07-13 01:34:08 +0800

    [NMP] 1717 reference answers done

--- leetcode/1717.js
+++ leetcode/1717.js
@@ -1,48 +1,69 @@
+/********* Need more practice *********/
+
+/*
+ * - [] Done
+ * - [x] Refer to what others are doing
+ *   - ⭐ greedy algorithm
+ */
 /**
  * @param {string} s
  * @param {number} x
  * @param {number} y
  * @return {number}
  */
+
+/* greedy + extra space for stack */
+/* T: O(n), S: O(n) */
 let maximumGain = (s, x, y) => {
-  let i = 0,
-    j = 1,
-    str = "",
-    last = -1,
-    score = 0;
+  let i = 0;
+  let score = 0;
+  let top, bottom;
+  let topScore, bottomScore;
 
-  while (1) {
-    if (!s[i] || !s[j]) break;
-
-    str = s[i] + s[j];
-
-    if (str === "ab") {
-      console.log("str === ab", i, j, x);
-      score += x;
-      j++;
-      last = i--;
-    } else if (str === "ba") {
-      console.log("str === ba", i, j, y);
-      score += y;
-      i--;
-      j++;
-      last = i--;
-    } else {
-      if (last >= 0) {
-        last = 0;
-        i = j;
-        j++;
-      } else {
-        i++;
-        j++;
-      }
-    }
+  if (x > y) {
+    top = "ab";
+    topScore = x;
+    bottom = "ba";
+    bottomScore = y;
+  } else {
+    top = "ba";
+    topScore = y;
+    bottom = "ab";
+    bottomScore = x;
   }
 
-  console.log(score);
+  console.log({ top, bottom });
+
+  let stack = [];
+  while (i < s.length) {
+    if (s[i] === top[1] && stack[stack.length - 1] === top[0]) {
+      score += topScore;
+      stack.pop();
+    } else {
+      stack.push(s[i]);
+    }
+    i++;
+  }
+
+  i = 0;
+  let newStack = [];
+  while (i < stack.length) {
+    if (stack[i] === bottom[1] && newStack[newStack.length - 1] === bottom[0]) {
+      score += bottomScore;
+      newStack.pop();
+    } else {
+      newStack.push(stack[i]);
+    }
+    i++;
+  }
 
   return score;
 };
 
+maximumGain("aabbaaxybbaabb", 5, 4);
+
+maximumGain("bcbcbbaaabab", 4, 5);
+
+maximumGain("cbbcbbaaabab", 4, 5);
+
 maximumGain("cdbcbbaaabab", 4, 5);
-// maximumGain("aabbaaxybbaabb", 5, 4);
