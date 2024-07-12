78a0674a5fd437bb370e05d63c3882a02b9a59f4
arthur <arthur.oceanus@gmail.com>
2024-07-13 02:27:01 +0800

    [NMP] 1717 reference answer
    O(n)
    O(n), modify string directly but still using O(n) space in javascript

--- leetcode/1717.js
+++ leetcode/1717.js
@@ -60,6 +60,57 @@ let maximumGain = (s, x, y) => {
   return score;
 };
 
+/* greedy */
+/* T: O(n)
+ * S: O(n), string is immutable should needs convert to array that is O(n) in javascript, otherwise O(1) in other languages like c++
+ * */
+maximumGain = (s, x, y) => {
+  const removePairs = (s, target) => {
+    let write_ind = 0,
+      count = 0;
+    s = s.split("");
+    for (let i = 0; i < s.length; i++) {
+      s[write_ind] = s[i];
+      write_ind++;
+      if (
+        write_ind >= 2 &&
+        s[write_ind - 1] == target[1] &&
+        s[write_ind - 2] == target[0]
+      ) {
+        count++;
+        write_ind -= 2;
+      }
+    }
+    s = s.slice(0, write_ind).join("");
+    return { s, count };
+  };
+
+  let res = 0;
+  let top, bot;
+  let top_score, bot_score;
+
+  if (y > x) {
+    top = "ba";
+    top_score = y;
+    bot = "ab";
+    bot_score = x;
+  } else {
+    top = "ab";
+    top_score = x;
+    bot = "ba";
+    bot_score = y;
+  }
+
+  let result = removePairs(s, top);
+  s = result.s;
+  res += result.count * top_score;
+
+  result = removePairs(s, bot);
+  res += result.count * bot_score;
+
+  return res;
+};
+
 maximumGain("aabbaaxybbaabb", 5, 4);
 
 maximumGain("bcbcbbaaabab", 4, 5);
