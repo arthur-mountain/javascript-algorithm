fcc6be1e15c939617fcb4b93547fd373bd595a27
arthur <arthur.oceanus@gmail.com>
2024-07-13 02:28:06 +0800

    [NMP] 1717 reference answer
    O(n), O(1), just using 4~5 variables

--- leetcode/1717.js
+++ leetcode/1717.js
@@ -111,6 +111,41 @@ maximumGain = (s, x, y) => {
   return res;
 };
 
+/* refer answer*/
+/* T: O(n), S: O(1) */
+maximumGain = (s, x, y) => {
+  let aCount = 0;
+  let bCount = 0;
+  let lesser = Math.min(x, y);
+  let result = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let c = s.charAt(i);
+    if (c > "b") {
+      result += Math.min(aCount, bCount) * lesser;
+      aCount = 0;
+      bCount = 0;
+    } else if (c === "a") {
+      if (x < y && bCount > 0) {
+        bCount--;
+        result += y;
+      } else {
+        aCount++;
+      }
+    } else if (c === "b") {
+      if (x > y && aCount > 0) {
+        aCount--;
+        result += x;
+      } else {
+        bCount++;
+      }
+    }
+  }
+
+  result += Math.min(aCount, bCount) * lesser;
+  return result;
+};
+
 maximumGain("aabbaaxybbaabb", 5, 4);
 
 maximumGain("bcbcbbaaabab", 4, 5);
