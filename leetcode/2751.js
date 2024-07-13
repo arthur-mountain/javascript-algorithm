814530b85886832c11e71a9208c3d2f8841231ae
arthur <arthur.oceanus@gmail.com>
2024-07-13 17:34:17 +0800

    [note] 2751

--- leetcode/2751.js
+++ leetcode/2751.js
@@ -96,6 +96,11 @@ let survivedRobotsHealths = (positions, healths, directions) => {
 };
 
 /* refer answer */
+/* this approach will using more memory,
+ * cause that using stack + extra two dimensions array robots([positions, healths, directions, index][])
+ *
+ * improvement will using only stack and one dimension array(indices[]) robots in below approach
+ */
 survivedRobotsHealths = (positions, healths, directions) => {
   // step1: sorting robots by positions
   let robots = [];
@@ -173,51 +178,51 @@ survivedRobotsHealths = (positions, healths, directions) => {
 
 /* refer answer */
 /* T: O(n logn), S: O(n)*/
-// survivedRobotsHealths = (positions, healths, directions) => {
-//   const n = positions.length;
-//
-//   // Create an array of indices from 0 to n-1
-//   const indices = Array.from({ length: n }, (_, i) => i);
-//
-//   // Sort indices based on the positions
-//   indices.sort((a, b) => positions[a] - positions[b]);
-//
-//   // Stack to keep track of robots moving to the right
-//   const stack = [];
-//
-//   for (const i of indices) {
-//     // If the robot is moving to the right, add it to the stack
-//     if (directions[i] === "R") {
-//       stack.push(i);
-//       continue;
-//     }
-//
-//     // If the robot is moving to the left, handle collisions
-//     while (stack.length > 0 && healths[i] > 0) {
-//       const top = stack[stack.length - 1];
-//
-//       // If the health of the top robot in the stack is less than the current robot's health
-//       if (healths[top] < healths[i]) {
-//         // Remove the robot from the stack and decrease the current robot's health by 1
-//         healths[top] = 0;
-//         stack.pop();
-//         healths[i] -= 1;
-//       } else if (healths[top] > healths[i]) {
-//         // Decrease the health of the top robot in the stack by 1 and set the current robot's health to 0
-//         healths[top] -= 1;
-//         healths[i] = 0;
-//       } else {
-//         // If both robots have the same health, remove both from the line
-//         healths[top] = 0;
-//         stack.pop();
-//         healths[i] = 0;
-//       }
-//     }
-//   }
-//
-//   // Collect the healths of the surviving robots
-//   return healths.filter((h) => h > 0);
-// };
+survivedRobotsHealths = (positions, healths, directions) => {
+  const n = positions.length;
+
+  // Create an array of indices from 0 to n-1
+  const indices = Array.from({ length: n }, (_, i) => i);
+
+  // Sort indices based on the positions
+  indices.sort((a, b) => positions[a] - positions[b]);
+
+  // Stack to keep track of robots moving to the right
+  const stack = [];
+
+  for (const i of indices) {
+    // If the robot is moving to the right, add it to the stack
+    if (directions[i] === "R") {
+      stack.push(i);
+      continue;
+    }
+
+    // If the robot is moving to the left, handle collisions
+    while (stack.length > 0 && healths[i] > 0) {
+      const top = stack[stack.length - 1];
+
+      // If the health of the top robot in the stack is less than the current robot's health
+      if (healths[top] < healths[i]) {
+        // Remove the robot from the stack and decrease the current robot's health by 1
+        healths[top] = 0;
+        stack.pop();
+        healths[i] -= 1;
+      } else if (healths[top] > healths[i]) {
+        // Decrease the health of the top robot in the stack by 1 and set the current robot's health to 0
+        healths[top] -= 1;
+        healths[i] = 0;
+      } else {
+        // If both robots have the same health, remove both from the line
+        healths[top] = 0;
+        stack.pop();
+        healths[i] = 0;
+      }
+    }
+  }
+
+  // Collect the healths of the surviving robots
+  return healths.filter((h) => h > 0);
+};
 
 survivedRobotsHealths([5, 4, 3, 2, 1], [2, 17, 9, 15, 10], "RRRRR");
 survivedRobotsHealths([3, 5, 2, 6], [10, 10, 15, 12], "RLRL");
