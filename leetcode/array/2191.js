b555b2b05aa0d7b3da957b9b1a622f72849bcba7
arthur <arthur.oceanus@gmail.com>
2024-07-24 14:50:12 +0800

    [follow up] 2191

--- leetcode/array/2191.js
+++ leetcode/array/2191.js
@@ -3,8 +3,12 @@
  *   - replace each digit with the mapping number,
  *   - convert the digits in array to number, also removing the leading zeros, e.g., 0007 -> 7
  *   - sort the original array by our map
+ * - [x] Follow up solutions
+ *   - same concept but using nums sort directly with a mapped helper func
+ *   - without extra memory space
  */
 /**
+ *
  * @param {number[]} mapping
  * @param {number[]} nums
  * @return {number[]}
@@ -23,6 +27,17 @@ let sortJumbled = (mapping, nums) => {
   return nums.sort((a, b) => map[a] - map[b]);
 };
 
+/* follow up */
+sortJumbled = (mapping, nums) => {
+  function mapped(num) {
+    let mappedVal = "";
+    for (let n of num.toString()) {
+      mappedVal += mapping[+n];
+    }
+    return +mappedVal;
+  }
+  return nums.sort((a, b) => mapped(a) - mapped(b));
+};
 
 sortJumbled([8, 9, 4, 0, 2, 1, 3, 5, 7, 6], [991, 338, 38]);
 sortJumbled([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [789, 456, 123]);
