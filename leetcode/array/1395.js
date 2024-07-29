ae008dc17d0699c6957f06bc6eda2123d2c90e41
arthur <arthur.oceanus@gmail.com>
2024-07-29 22:29:38 +0800

    [wip] 1395 v2(failed)
    time limit exceeded

--- leetcode/array/1395.js
+++ leetcode/array/1395.js
@@ -2,25 +2,58 @@
  * @param {number[]} rating
  * @return {number}
  */
-let numTeams = (rating) => {
-};
+let numTeams = (rating) => {};
 
 /* problem-solving by violence but `Time Limit Exceeded` */
-numTeams = (rating) => {
-  let count = 0;
+// numTeams = (rating) => {
+//   let count = 0;
+//
+//   for (let i = 0; i < rating.length; i++) {
+//     for (let j = i + 1; j < rating.length; j++) {
+//       for (let k = j + 1; k < rating.length; k++) {
+//         if (
+//           (rating[i] < rating[j] && rating[j] < rating[k]) ||
+//           (rating[i] > rating[j] && rating[j] > rating[k])
+//         ) {
+//           count++;
+//         }
+//       }
+//     }
+//   }
+//
+//   return count;
+// };
 
-  for (let i = 0; i < rating.length; i++) {
-    for (let j = i + 1; j < rating.length; j++) {
-      for (let k = j + 1; k < rating.length; k++) {
-        if (
-          (rating[i] < rating[j] && rating[j] < rating[k]) ||
-          (rating[i] > rating[j] && rating[j] > rating[k])
-        ) {
-          count++;
-        }
-      }
-    }
-  }
+/* problem-solving by violence but `Time Limit Exceeded` */
+// numTeams = (rating) => {
+//   let count = 0;
+//
+//   for (let i = 0; ; i++) {
+//     if (i >= rating.length - 2) break;
+//
+//     let j = i + 1;
+//     let k = j + 1;
+//
+//     while (k <= rating.length) {
+//       if (
+//         (rating[i] < rating[j] && rating[j] < rating[k]) ||
+//         (rating[i] > rating[j] && rating[j] > rating[k])
+//       ) {
+//         count++;
+//       }
+//
+//       k++;
+//
+//       if (k > rating.length) {
+//         j++;
+//         k = j + 1;
+//       }
+//     }
+//   }
+//
+//   return count;
+// };
 
-  return count;
-};
+numTeams([2, 5, 3, 4, 1]);
+// numTeams([2, 1, 3]);
+// numTeams([1, 2, 3, 4]);
