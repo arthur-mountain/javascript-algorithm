d2aa658a0259ce6d6fcf1aaefc44baa5da7c2870
arthur <arthur.oceanus@gmail.com>
2024-08-01 12:39:20 +0800

    [done] 2678 v2(accepted)

--- leetcode/2678.js
+++ leetcode/2678.js
@@ -26,5 +26,33 @@ let countSeniors = (details) => {
   return count;
 };
 
+countSeniors = (details) => {
+  /**
+   * the details[i].length == 15
+   * the last 4 characters represent the age and the seat allotted
+   *
+   * we cound found the first two characters of 4 characters from the end
+   *
+   * if (detail[11] > 6) {
+   *   count++
+   *   continue
+   * }
+   *
+   * if(detail[11] == 6 && detail[12] > 0)count++
+   **/
+  let count = 0;
+
+  for (const detail of details) {
+    if (+detail[11] > 6) {
+      count++;
+      continue;
+    }
+
+    if (+detail[11] >= 6 && +detail[12] > 0) count++;
+  }
+
+  return count;
+};
+
 countSeniors(["7868190130M7522", "5303914400F9211", "9273338290F4010"]);
 countSeniors(["1313579440F2036", "2921522980M5644"]);
