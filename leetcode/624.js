bb2a7e819e4d820fe1f175fbec76482239a7905d
arthur <arthur.oceanus@gmail.com>
2024-08-17 15:11:51 +0800

    [done/refer-ansewr] 624 v4(accepted)

--- leetcode/624.js
+++ leetcode/624.js
@@ -1,6 +1,7 @@
 /*
- * - [] Done
- * - [] Follow up solutions
+ * - [x] Done
+ * - [x] Follow up solutions
+ *     the done answer from reference solutions
  */
 /**
  * @param {number[][]} arrays
@@ -8,48 +9,41 @@
  */
 let maxDistance = (arrays) => {
   /*
-   * brute force, pick the each array and compare with the rest of the arrays
+   * 記錄 iteration 過程中的最小值和最大值
    *
-   * time limit exceeded
+   *  並且記錄當前最大距離，在每次 iteration 過程中，取得當前的最大距離，根據：
+   *  1. 當前的最大距離
+   *  2. 「iteration 過程中的最大值」減去「當前陣列中的最小值」
+   *  3. 「當前陣列中的最大值」      減去「iteration 過程中的最小值」
+   *
+   * 更新目前 iteration 過程中的最小值和最大值
+   *
+   * 可以直接更新 itration 過程中的最小值和最大值，不用擔心在同一個陣列中選取相同的值
+   * 因為，
+   * 每次 itration 都是拿尚未遍歷過的 array 中的最小、最大值 跟 itration 過的 array 的最小、最大值做比較，
    * */
+  let currMin = arrays[0][0];
+  let currMax = arrays[0][arrays[0].length - 1];
+  let maxDistance = 0;
 
-  let max = 0;
-  for (let i = 0; i < arrays.length; i++) {
-    for (let j = 0; j < arrays.length; j++) {
-      if (i === j) continue;
-      max = Math.max(
-        max,
-        Math.abs(
-          arrays[i][arrays[i].length - 1] - arrays[j][0],
-          arrays[i][0] - arrays[j][arrays[j].length - 1],
-        ),
-      );
-    }
+  for (let i = 1, len = arrays.length; i < len; i++) {
+    const currArrLen = arrays[i].length;
+    // found the maxDistance,
+    // 1. current max distance
+    // 2. current array min subtract currMax
+    // 3. current array max subtract currMin
+    maxDistance = Math.max(
+      maxDistance,
+      Math.abs(currMax - arrays[i][0]),
+      Math.abs(arrays[i][currArrLen - 1] - currMin),
+    );
+
+    // update current min and max with current array
+    // we can update the min and max directly,
+    // do not afraid of pick same value in same array,
+    currMin = Math.min(currMin, arrays[i][0]);
+    currMax = Math.max(currMax, arrays[i][currArrLen - 1]);
   }
-  return max;
+
+  return maxDistance;
 };
-
-// maxDistance([
-//   [1, 2, 3],
-//   [4, 5],
-//   [1, 2, 3],
-// ]);
-//
-// maxDistance([[1], [1]]);
-
-maxDistance([
-  [-1, 1],
-  [-3, 1, 4],
-  [-2, -1, 0, 2],
-]);
-
-maxDistance([
-  [-1, 1],
-  [-3, 1, 4],
-  [-2, -1, 0, 2],
-]);
-
-maxDistance([
-  [1, 5],
-  [3, 4],
-]);
