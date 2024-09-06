ac92bb90a67cf22cdfebe7ecb0a072e44574a1ce
arthur <arthur.oceanus@gmail.com>
2024-09-06 14:58:26 +0800

    [done] 3217 v1(accepted)

---
+++ leetcode/link-list/3217.js
@@ -0,0 +1,37 @@
+/*
+ * - [x] Done
+ * - [] Follow up solutions
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+let modifiedList = (nums, head) => {
+  nums = new Set(nums);
+
+  let prev = null;
+  let current = head;
+  while (current) {
+    if (nums.has(current.val)) {
+      if (prev) {
+        prev.next = current.next;
+        current = current.next;
+      } else {
+        current = current.next;
+        head = current;
+      }
+    } else {
+      prev = current;
+      current = current.next;
+    }
+  }
+  return head;
+};
