fd6de998749339d92c8ad0e175ff31943abb0218
arthur <arthur.oceanus@gmail.com>
2024-09-17 16:51:46 +0800

    [followup] 884 v1(accepted)

--- leetcode/884.js
+++ leetcode/884.js
@@ -29,8 +29,41 @@ let uncommonFromSentences = (s1, s2) => {
   // End time complexity: O(n + m)
   return [...map.entries()]
     .filter(([_, count]) => count === 1)
-    .map(([word, _]) => word);
+    .map(([word, _]) => word); // this create multiple time new arrays
 
   // Final Time complexity: O(n + m)
   // Final Space complexity: O(n + m)
+};
+
+/** follwup: this is better than first implementation (less memory usage)*/
+uncommonFromSentences = (s1, s2) => {
+  // Space complexity: O(n + m)
+  const map = new Map();
+
+  // Time complexity: O(n) for split s1
+  // Time complexity: O(1) for map get and set
+  for (const word of s1.split(" ")) {
+    map.set(word, map.get(word) + 1 || 1);
+  }
+
+  // Time complexity: O(m) for split s2
+  // Time complexity: O(1) for map get and set
+  for (const word of s2.split(" ")) {
+    map.set(word, map.get(word) + 1 || 1);
+  }
+
+  // Space complexity: O(n + m)
+  const result = [];
+
+  // Time complexity: O(n + m) for map transform to entries
+  for (const [word, count] of map.entries()) {
+    if (count === 1) {
+      // Time complexity: O(1) for push to last position does not shift front elements
+      result.push(word);
+    }
+  }
+
+  return result;
+  // Final time complexity: O(n + m)
+  // Final space complexity: O(n + m)
 };
