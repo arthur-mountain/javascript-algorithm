23b13eb0d35e23ccc1c8f2483f7f8c574a93bf9d
arthur <arthur.oceanus@gmail.com>
2024-10-02 16:34:55 +0800

    [done] 1331 v1(accepted)

---
+++ leetcode/1331.js
@@ -0,0 +1,40 @@
+/*
+ * - [x] Done
+ * - [] Follow up solutions
+ */
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+let arrayRankTransform = (arr) => {
+  /**
+   * Store each integer and its index in an array [[int, index], ...].
+   * Sort the integers in ascending order.
+   * Reassign the rank (index after sorting) to the original index in arr.
+   * If the integer is the same as the previous one, keep the same rank.
+   * Otherwise, assign the new rank to the index.
+   **/
+
+  // Space: O(n)
+  let temp = [];
+
+  // Time: O(n)
+  for (let i = 0; i < arr.length; i++) {
+    temp.push([arr[i], i]);
+  }
+
+  // Time: O(nlogn)
+  temp.sort((a, b) => a[0] - b[0]);
+
+  // Time: O(n)
+  for (let i = 0; i < temp.length; i++) {
+    arr[temp[i][1]] =
+      temp[i]?.[0] === temp[i - 1]?.[0]
+        ? arr[temp[i - 1][1]]
+        : (arr[temp[i - 1]?.[1]] ?? i) + 1;
+  }
+
+  // Total Time  Complexity: O(n) + O(nlogn) + O(n) => O(nlogn)
+  // Total Space Complexity: O(n)
+  return arr;
+};
