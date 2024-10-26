38c3923604a0cd0f66e3193b70ce1e4c12aedb13
arthur <arthur.oceanus@gmail.com>
2024-10-26 15:54:43 +0800

    2458 [time limit exceeded][v2]

--- leetcode/2458.js
+++ leetcode/2458.js
@@ -17,16 +17,16 @@
  */
 let treeQueries = (root, queries) => {
   /**
-       * Removed the rooted at node value is $query from tree,
-       * calc the max height of current tree,
-       * repeating the processing, until the all queries visited
-  
-       * Algorithm:
-          1. if query is the root node, reutrn 0
-  
-       *  2. calc the height with BFS.
-              2.1. if the node.val == $query, do not push the node to queue.
-       */
+     * Removed the rooted at node value is $query from tree,
+     * calc the max height of current tree, 
+     * repeating the processing, until the all queries visited 
+
+     * Algorithm:
+        1. if query is the root node, reutrn 0
+
+     *  2. calc the height with BFS.
+     */
+
   const helper = (root, query) => {
     // constraints: query not allowed equals to  root.val
     // if (root.val === query) {
@@ -37,9 +37,14 @@ let treeQueries = (root, queries) => {
     while (queue.length) {
       for (let idx = 0, len = queue.length; idx < len; idx++) {
         const node = queue.shift();
-        if (node.val === query) continue;
-        if (node.left) queue.push(node.left);
-        if (node.right) queue.push(node.right);
+        // constraints: query not allowed equals to root.val so we don't need to check here
+        // if (node.val === query) continue;
+        if (node.left && node.left.val !== query) {
+          queue.push(node.left);
+        }
+        if (node.right && node.right.val !== query) {
+          queue.push(node.right);
+        }
       }
       if (queue.length) {
         height++;
