9a43045dca280e8f53f89b8d86e23c5b3fcd33ee
arthur <arthur.oceanus@gmail.com>
2024-10-27 15:28:58 +0800

    21 [done][v1]

---
+++ leetcode/link-list/21.js
@@ -0,0 +1,52 @@
+/*
+ * - [x] Done
+ * - [] Follow up solutions
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+let mergeTwoLists = (list1, list2) => {
+  let dummy = new ListNode(0);
+  let tail = dummy;
+
+  while (list1 && list2) {
+    if (list1.val > list2.val) {
+      tail.next = list2;
+      list2 = list2.next;
+      tail = tail.next;
+    } else if (list1.val < list2.val) {
+      tail.next = list1;
+      list1 = list1.next;
+      tail = tail.next;
+    } else {
+      tail.next = list1;
+      list1 = list1.next;
+      tail.next.next = list2;
+      list2 = list2.next;
+      tail = tail.next.next;
+    }
+  }
+
+  while (list1) {
+    tail.next = list1;
+    tail = list1;
+    list1 = list1.next;
+  }
+
+  while (list2) {
+    tail.next = list2;
+    tail = list2;
+    list2 = list2.next;
+  }
+
+  return dummy.next;
+};
