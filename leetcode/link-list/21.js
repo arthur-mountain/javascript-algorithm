3b808d4c1ee82f39baabc12f2586893719a392f1
arthur <arthur.oceanus@gmail.com>
2024-10-27 15:34:29 +0800

    21 [follow up][v3]

--- leetcode/link-list/21.js
+++ leetcode/link-list/21.js
@@ -1,6 +1,6 @@
 /*
  * - [x] Done
- * - [] Follow up solutions
+ * - [x] Follow up solutions
  */
 /**
  * Definition for singly-linked list.
@@ -76,6 +76,36 @@ mergeTwoLists = (list1, list2) => {
       list2 = list2.next;
       tail = tail.next.next;
     }
+  }
+
+  if (list1) {
+    tail.next = list1;
+  } else if (list2) {
+    tail.next = list2;
+  }
+
+  return dummy.next;
+};
+
+/**
+ * follow up:
+ * simplify the code by removing the else if condition when both of list exists. cause
+ * if the val is equal, we just one of node in the both list to dummy.next,
+ * we no needs to check else if condition, like previous solutions.
+ */
+mergeTwoLists = (list1, list2) => {
+  let dummy = new ListNode(0);
+  let tail = dummy;
+
+  while (list1 && list2) {
+    if (list1.val > list2.val) {
+      tail.next = list2;
+      list2 = list2.next;
+    } else {
+      tail.next = list1;
+      list1 = list1.next;
+    }
+    tail = tail.next;
   }
 
   if (list1) {
