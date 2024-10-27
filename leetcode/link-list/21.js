9349bec051284ebcb9b7f317bf838b60767f8188
arthur <arthur.oceanus@gmail.com>
2024-10-27 15:54:01 +0800

    21 [follow up][v5]

--- leetcode/link-list/21.js
+++ leetcode/link-list/21.js
@@ -135,3 +135,39 @@ mergeTwoLists = (l1, l2) => {
     return l2;
   }
 };
+
+/*
+ * follow to:
+ * access pointer directly without extra dummy node
+ **/
+mergeTwoLists = (l1, l2) => {
+  if (!l1) return l2;
+  if (!l2) return l1;
+
+  let head;
+
+  if (l1.val < l2.val) {
+    head = l1;
+    l1 = l1.next;
+  } else {
+    head = l2;
+    l2 = l2.next;
+  }
+
+  curr = head;
+
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      curr.next = l1;
+      l1 = l1.next;
+    } else {
+      curr.next = l2;
+      l2 = l2.next;
+    }
+    curr = curr.next;
+  }
+
+  curr.next = l1 || l2;
+
+  return head;
+};
