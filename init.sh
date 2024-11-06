7c7f45f4aff63d77af92442bcfc10f37d15a11ad
arthur <arthur.oceanus@gmail.com>
2024-11-06 16:56:57 +0800

    update template script

---
+++ init.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z "$1" ]; then
+  echo "Error: No filename provided. Usage: ./script.sh <filename>"
+  exit 1
+fi
+
+FILE_PATH="leetcode/$1.js"
+
+if [ -f "$FILE_PATH" ]; then
+  echo "Error: File '$FILE_PATH' already exists."
+  exit 1
+fi
+
+# 創建檔案並插入模板內容
+cat >"$FILE_PATH" <<-EOF
+/**
+ * Status:
+ *  - [ ] Done
+ *  - [ ] Follow-up solutions
+ *
+ * Title:
+ *
+ * Topics:
+ *
+ * Constraints:
+ *
+ * Statements:
+ **/
+EOF
+
+echo "File '$FILE_PATH' created successfully."
