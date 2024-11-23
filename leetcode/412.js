cd3632097b451ad0e65caea7c8e2b0bba62f27fd
arthur <arthur.oceanus@gmail.com>
2024-11-23 08:56:43 +0800

    412 [done][v1]

---
+++ leetcode/412.js
@@ -0,0 +1,55 @@
+/**
+ * Status:
+ *  - [x] Done
+ *  - [ ] Follow-up solutions
+ *
+ * Title:
+ *    412. Fizz Buzz
+ *
+ * Topics:
+ *    1. Math
+ *
+ *    2. String
+ *
+ *    3. Simulation
+ *
+ * Constraints:
+ *    1. 1 <= n <= 10^4
+ *
+ * Statements:
+ *    Given an integer n, return a string array answer (1-indexed) where:
+ *
+ *    answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+ *
+ *    answer[i] == "Fizz" if i is divisible by 3.
+ *
+ *    answer[i] == "Buzz" if i is divisible by 5.
+ *
+ *    answer[i] == i (as a string) if none of the above conditions are true.
+ *
+ *    note: the answer must be 1-indexed.
+ **/
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+let fizzBuzz = (n) => {
+  // Space: O(n)
+  let answer = Array({ length: n });
+
+  // Time: O(n)
+  for (let i = 1, idx = 0; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      answer[idx++] = "FizzBuzz";
+    } else if (i % 3 === 0) {
+      answer[idx++] = "Fizz";
+    } else if (i % 5 === 0) {
+      answer[idx++] = "Buzz";
+    } else {
+      answer[idx++] = `${i}`;
+    }
+  }
+
+  return answer;
+};
