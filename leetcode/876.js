be14307ffa66720904949ad00fe01ac424f5c493
arthur <arthur.oceanus@gmail.com>
2024-11-23 10:21:21 +0800

    876 [done][v2]

--- leetcode/876.js
+++ leetcode/876.js
@@ -9,6 +9,8 @@
  * Topics:
  *    1. Linked List
  *
+ *    2. Two Pointers
+ *
  * Statements:
  *   Given a non-empty, singly linked list with head node, return a middle node of linked list.
  *
@@ -61,4 +63,24 @@ let middleNode = (head) => {
     count++;
     current = current.next;
   }
+};
+
+middleNode = (head) => {
+  /*
+   * Thoughts:
+   *  using fast/slow pointer,
+   *  the slow pointer go 1 step each time,
+   *  the fast pointer go 2 steps each time,
+   *  when the fast pointer reach the end of the list, the slow pointer will be at the middle of the list.
+   *
+   *  Space: O(1)
+   *  Time: O(log(n))
+   **/
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
 };
