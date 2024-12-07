3dd576b229e8720605fb8131796234f64e4ffdfc
arthur <arthur.oceanus@gmail.com>
2024-12-07 21:23:55 +0800

    1295 [followup][v1]

--- leetcode/1295.js
+++ leetcode/1295.js
@@ -1,7 +1,7 @@
 /**
  * Status:
  *  - [x] Done
- *  - [ ] Follow-up solutions
+ *  - [x] Follow-up solutions
  *
  * Title:
  *   1295. Find Numbers with Even Number of Digits
@@ -15,7 +15,7 @@
  *    return how many of them contain an even number of digits.
  *
  * Constraints:
- *    1.  1 <= nums.length <= 500
+ *    1. 1 <= nums.length <= 500
  *    2. 1 <= nums[i] <= 105
  **/
 let findNumbers = (nums) => {
@@ -33,6 +33,22 @@ findNumbers = (nums) => {
 
   for (const num of nums) {
     if ((`${num}`.length & 1) === 0) count++;
+  }
+
+  return count;
+};
+
+// follow up: count the number of digits manually, without converting to string
+findNumbers = (nums) => {
+  let count = 0;
+
+  for (let num of nums) {
+    let digit_count = 0;
+    while (num) {
+      digit_count++;
+      num = Math.floor(num / 10);
+    }
+    if ((digit_count & 1) == 0) count++;
   }
 
   return count;
