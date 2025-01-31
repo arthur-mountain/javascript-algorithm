0a3b091929455a923792402ddb9f73f386be6263
arthur <arthur.oceanus@gmail.com>
2025-02-01 02:21:05 +0800

    [data-structures]: custom array update

--- data-structures/Array/custom-array.js
+++ data-structures/Array/custom-array.js
@@ -66,4 +66,36 @@ class CustomArray {
   }
 }
 
+class CustomArrayWithExtends extends StaticArray {
+  #intervalId;
+
+  constructor() {
+    super();
+    this.#sizeChecker();
+  }
+
+  #sizeChecker() {
+    if (this.#intervalId) return;
+    this.#intervalId = setInterval(() => {
+      if (this.size() < this.capacity() / 2) {
+        this.shrink();
+      }
+    }, 60 * 1000);
+  }
+
+  destroy() {
+    if (this.#intervalId) {
+      clearInterval(this.#intervalId);
+      this.#intervalId = null;
+    }
+  }
+
+  insert(index, element) {
+    super.insert(index, element);
+    if (this.isFull()) {
+      this.extend();
+    }
+  }
+}
+
 export { CustomArray };
