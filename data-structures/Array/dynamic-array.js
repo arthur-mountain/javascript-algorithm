3f42206882c0b86b246f425b3feb11b2b3f3087b
arthur <arthur.oceanus@gmail.com>
2025-02-02 15:29:00 +0800

    [data-structures]: dynamic array update

--- data-structures/Array/dynamic-array.js
+++ data-structures/Array/dynamic-array.js
@@ -1,57 +1,13 @@
 const StaticArray = require("./static-array");
 
-class DynamicArray {
-  #staticArray;
-
+class DynamicArray extends StaticArray {
   constructor() {
-    this.#staticArray = new StaticArray();
-  }
-
-  isEmpty() {
-    return this.#staticArray.isEmpty();
-  }
-
-  isFull() {
-    return this.#staticArray.isFull();
-  }
-
-  isEmpty() {
-    return this.#staticArray.isEmpty();
-  }
-
-  capacity() {
-    return this.#staticArray.capacity();
-  }
-
-  size() {
-    return this.#staticArray.size();
-  }
-
-  get(index) {
-    return this.#staticArray.get(index);
-  }
-
-  set(index, element) {
-    this.#staticArray.set(index, element);
-  }
-
-  delete(index) {
-    this.#staticArray.delete(index);
-  }
-
-  clear() {
-    this.#staticArray.clear();
-  }
-
-  printAll() {
-    this.#staticArray.printAll();
+    super();
   }
 
   insert(index, element) {
-    this.#staticArray.insert(index, element);
-    if (this.#staticArray.isFull()) {
-      this.#staticArray.extend();
-    }
+    super.insert(index, element);
+    this.isFull() && this.extend();
   }
 }
 
