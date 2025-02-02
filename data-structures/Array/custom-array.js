4a1df85002a927c1470586b2dc72ba4c13daca4b
arthur <arthur.oceanus@gmail.com>
2025-02-02 15:27:43 +0800

    [data-structures]: custom array update

--- data-structures/Array/custom-array.js
+++ data-structures/Array/custom-array.js
@@ -1,72 +1,6 @@
 const StaticArray = require("./static-array");
 
-class CustomArray {
-  #intervalId;
-  #staticArray;
-
-  constructor() {
-    this.#staticArray = new StaticArray();
-    this.#sizeChecker();
-  }
-
-  #sizeChecker() {
-    if (this.#intervalId) return;
-    this.#intervalId = setInterval(() => {
-      if (this.#staticArray.size() < this.#staticArray.capacity() / 2) {
-        this.#staticArray.shrink();
-      }
-    }, 60 * 1000);
-  }
-
-  isEmpty() {
-    return this.#staticArray.isEmpty();
-  }
-
-  isFull() {
-    return this.#staticArray.isFull();
-  }
-
-  isEmpty() {
-    return this.#staticArray.isEmpty();
-  }
-
-  capacity() {
-    return this.#staticArray.capacity();
-  }
-
-  size() {
-    return this.#staticArray.size();
-  }
-
-  get(index) {
-    return this.#staticArray.get(index);
-  }
-
-  set(index, element) {
-    this.#staticArray.set(index, element);
-  }
-
-  delete(index) {
-    this.#staticArray.delete(index);
-  }
-
-  clear() {
-    this.#staticArray.clear();
-  }
-
-  printAll() {
-    this.#staticArray.printAll();
-  }
-
-  insert(index, element) {
-    this.#staticArray.insert(index, element);
-    if (this.#staticArray.isFull()) {
-      this.#staticArray.extend();
-    }
-  }
-}
-
-class CustomArrayWithExtends extends StaticArray {
+class CustomArray extends StaticArray {
   #intervalId;
 
   constructor() {
@@ -92,9 +26,7 @@ class CustomArrayWithExtends extends StaticArray {
 
   insert(index, element) {
     super.insert(index, element);
-    if (this.isFull()) {
-      this.extend();
-    }
+    this.isFull() && this.extend();
   }
 }
 
