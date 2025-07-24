7c2af9a1a0cdc382356fc96da9e1f768547f397e
arthur <arthur.oceanus@gmail.com>
2025-07-24 20:20:44 +0800

    700 [done]

---
+++ leetcodes/700.js
@@ -0,0 +1,53 @@
+/**
+ * Status:
+ *    - [x] Done
+ *    - [ ] Follow-up solutions
+ *
+ * Title:
+ *    700. Search in a Binary Search Tree
+ *
+ * Topics:
+ *    1. Tree
+ *    2. Binary Search Tree
+ *    3. Binary Tree
+ *
+ * Statements:
+ *   從一顆 Binary Search Tree 找到 val
+ *
+ * Constraints:
+ *    1. The number of nodes in the tree is in the range [1, 5000].
+ *    2. 1 <= Node.val <= 10**7
+ *    3. root is a binary search tree.
+ *    4. 1 <= val <= 10**7
+ **/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function (root, val) {
+  /* Iteration */
+  while (root != null) {
+    if (root.val === val) {
+      return root;
+    }
+
+    if (root.val > val) {
+      root = root.left;
+    } else {
+      root = root.right;
+    }
+  }
+  return null;
+  // Time : O(logn)
+  // Space: O(1)
+};
