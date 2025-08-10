7f582928cc4983771d9e03944519bcf86edcd664
arthur <arthur.oceanus@gmail.com>
2025-08-10 15:31:23 +0800

    700 update comments

--- leetcodes/700.js
+++ leetcodes/700.js
@@ -36,18 +36,25 @@
  */
 var searchBST = function (root, val) {
   /* Iteration */
+
+  // 當前節點不為空時
   while (root != null) {
+    // 要找的值等於當前的值，代表找到了
     if (root.val === val) {
       return root;
     }
 
     if (root.val > val) {
+      // 當前的值 > 要找的值，代表要找的值**比較小**，所以往左邊找
       root = root.left;
     } else {
+      // 當前的值 <= 要找的值，代表要找的值**比較大**，所以往右邊找
       root = root.right;
     }
   }
+  // 上面迴圈結束，函式沒跳出，代表沒有找到(根節點為空 or 抵達葉子節點)
   return null;
+
   // Time : O(logn)
   // Space: O(1)
 };
@@ -55,13 +62,15 @@ var searchBST = function (root, val) {
 {
   var searchBST = function (root, val) {
     /* Recursion */
-    // root == null -> 葉子節點
+
+    // 前者判斷根節點是否為空又或者如果已經抵達葉子節點時，代表沒找到，回傳 null(當前於回傳當前節點)
+    // 後者判斷當前節點的值等於要找的值，代表找到了，回傳當前節點
     if (root == null || root.val === val) {
       return root;
     }
     return root.val > val
-      ? searchBST(root.left, val)
-      : searchBST(root.right, val);
+      ? searchBST(root.left, val) // 當前的值 > 要找的值，代表要找的值**比較小**，所以往左邊找
+      : searchBST(root.right, val); // 當前的值 <= 要找的值，代表要找的值**比較大**，所以往右邊找
     // Time : O(logn)
     // Space: O(logn)
   };
