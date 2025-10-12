99a2f1909f80d6717fcba909b18a410914c468d7
arthur <arthur.oceanus@gmail.com>
2025-10-12 22:47:01 +0800

    421 [tle] v2.1 - 進一步優化位元操作得實作

--- leetcodes/trie/421/solution2.js
+++ leetcodes/trie/421/solution2.js
@@ -22,7 +22,7 @@ var findMaximumXOR = function (nums) {
 
 class BinaryTrieNode {
   constructor() {
-    this.children = Array(2).fill(null);
+    this.children = [null, null];
   }
 }
 
@@ -40,11 +40,14 @@ class BinaryTrie {
    */
   insert(num) {
     let current = this.root;
-    for (let i = 0; i < 32; i++) {
-      const bit = (num >> (31 - i)) & 1; // 取得 num 的第 i 位（從左數）
+    for (let i = 31; i >= 0; i--) {
+      // ✅  取得 num 的第 i 位(從左數)，從後往前遍歷，避免取 bit 時的 31-i 計算
+      const bit = (num >> i) & 1;
+
       if (current.children[bit] === null) {
         current.children[bit] = new BinaryTrieNode();
       }
+
       current = current.children[bit];
     }
   }
@@ -54,14 +57,16 @@ class BinaryTrie {
    * Space: O(1)
    */
   findMaxXOR(num) {
-    let current = this.root;
     // 用於計算 MaxXOR 的值，核心概念：
     // << 1 ：將現有數字左移一位(相當於「騰出右邊一個位置」)
     // | bit：將新的 bit 填入最右邊
     let result = 0;
-    for (let i = 0; i < 32; i++) {
-      const bit = (num >> (31 - i)) & 1; // 取得 num 的第 i 位（從左數）
-      const opposite = bit === 0 ? 1 : 0;
+    let current = this.root;
+
+    for (let i = 31; i >= 0; i--) {
+      const bit = (num >> i) & 1; // 取得 num 的第 i 位(從左數)
+      const opposite = 1 - bit; // 另一種反向取值的方式，因為「1 - 0 = 1」「1 - 1 = 0」
+
       // 如果相反的 bit 不存在，則選原本的 bit，則 xor 出來的值會是 0
       if (current.children[opposite] === null) {
         result = (result << 1) | 0;
@@ -72,6 +77,7 @@ class BinaryTrie {
         current = current.children[opposite];
       }
     }
+
     return result;
   }
 }
