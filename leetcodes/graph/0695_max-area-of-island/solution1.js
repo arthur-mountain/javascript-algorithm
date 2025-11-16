6a03daf7562920d1527afdcc4a35cec93df1c259
arthur <arthur.oceanus@gmail.com>
2025-11-16 16:43:29 +0800

    695 [AC] - init dfs iteration

--- leetcodes/graph/0695_max-area-of-island/solution1.js
+++ leetcodes/graph/0695_max-area-of-island/solution1.js
@@ -33,3 +33,42 @@ var maxAreaOfIsland = function (grid) {
 
   return maxLandCount;
 };
+
+var maxAreaOfIslandIteration = function (grid) {
+  const row = grid.length;
+  const col = grid[0].length;
+  let maxLandCount = 0;
+
+  const dfs = (i, j, landCount) => {
+    const stack = [i, j];
+
+    while (stack.length > 0) {
+      j = stack.pop();
+      i = stack.pop();
+
+      if (i < 0 || i >= row || j < 0 || j >= col || grid[i][j] === 0) {
+        continue;
+      }
+
+      landCount++;
+      grid[i][j] = 0;
+
+      stack.push(i - 1, j); // 上
+      stack.push(i, j + 1); // 右
+      stack.push(i + 1, j); // 下
+      stack.push(i, j - 1); // 左
+    }
+
+    return landCount;
+  };
+
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      if (grid[i][j] === 1) {
+        maxLandCount = Math.max(maxLandCount, dfs(i, j, 0));
+      }
+    }
+  }
+
+  return maxLandCount;
+};
