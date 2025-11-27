b35e79d8605edb11c95d004f2c83c1a2dfce9947
arthur <arthur.oceanus@gmail.com>
2025-11-27 23:27:25 +0800

    542 dp 優化
    不論是在第一輪或第二輪迴圈中，只要當前節點是 0，
    那麼最短路徑一定是自己，可以不用處理

--- leetcodes/matrix/0542_01-matrix/solution3.js
+++ leetcodes/matrix/0542_01-matrix/solution3.js
@@ -13,7 +13,7 @@ var updateMatrix = function (mat) {
   // 只處理當前節點為 1 時的最短路徑，如果當前節點為 0，那最短路徑就會是 0，所以不用比較
   for (let row = 0; row < ROW; row++) {
     for (let col = 0; col < COL; col++) {
-      if (mat[row][col] === 1) {
+      if (mat[row][col] !== 0) {
         mat[row][col] =
           Math.min(
             // 左節點。也可以寫成「col - 1 >= 0 ? mat[row][col - 1] : Infinity」
@@ -30,13 +30,15 @@ var updateMatrix = function (mat) {
   // 當前節點(作為第一次遍歷左上的最短路徑的結果)跟右、下方節點比較，是否有再更短的路徑
   for (let row = ROW - 1; row >= 0; row--) {
     for (let col = COL - 1; col >= 0; col--) {
-      mat[row][col] = Math.min(
-        mat[row][col],
-        // 右節點。也可以寫成「col + 1 < COL ? mat[row][col + 1] + 1 : Infinity」
-        getSafeValue(row, col + 1) + 1, // 鄰居節點加一步才會是當前節點的值
-        // 下節點。也可以寫成「row + 1 < ROW ? mat[row + 1][col] + 1 : Infinity」
-        getSafeValue(row + 1, col) + 1, // 鄰居節點加一步才會是當前節點的值
-      );
+      if (mat[row][col] !== 0) {
+        mat[row][col] = Math.min(
+          mat[row][col],
+          // 右節點。也可以寫成「col + 1 < COL ? mat[row][col + 1] + 1 : Infinity」
+          getSafeValue(row, col + 1) + 1, // 鄰居節點加一步才會是當前節點的值
+          // 下節點。也可以寫成「row + 1 < ROW ? mat[row + 1][col] + 1 : Infinity」
+          getSafeValue(row + 1, col) + 1, // 鄰居節點加一步才會是當前節點的值
+        );
+      }
     }
   }
 
