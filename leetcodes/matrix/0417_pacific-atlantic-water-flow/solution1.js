552fcd66ab95f9d6b680290327692fe5a0b00d90
arthur <arthur.oceanus@gmail.com>
2026-02-22 19:32:07 +0800

    417 [AC] - improve solution1

--- leetcodes/matrix/0417_pacific-atlantic-water-flow/solution1.js
+++ leetcodes/matrix/0417_pacific-atlantic-water-flow/solution1.js
@@ -13,18 +13,15 @@ var pacificAtlantic = function (heights) {
   ];
 
   // 左上 -> 右下
-  const s1 = new Set(); // 收集可以回出發邊界的節點
-  const v1 = new Set(); // 拜訪過的節點
+  const v1 = new Set(); // 拜訪過的節點，同時也作為結果集
   const q1 = []; // 待處理的節點
   for (let row = 0; row < ROW; row++) {
     q1.push([row, 0]);
     v1.add(row * COL);
-    s1.add(row * COL);
   }
   for (let col = 0; col < COL; col++) {
     q1.push([0, col]);
     v1.add(col);
-    s1.add(col);
   }
   while (q1.length > 0) {
     const [row, col] = q1.shift();
@@ -47,24 +44,20 @@ var pacificAtlantic = function (heights) {
       }
 
       v1.add(key);
-      s1.add(key);
       q1.push([newRow, newCol]);
     }
   }
 
   // 右下 -> 左上
-  const s2 = new Set(); // 收集可以回出發邊界的節點
-  const v2 = new Set(); // 拜訪過的節點
+  const v2 = new Set(); // 拜訪過的節點，同時也作為結果集
   const q2 = []; // 待處理的節點
   for (let row = ROW - 1; row >= 0; row--) {
     q2.push([row, COL - 1]);
     v2.add(row * COL + COL - 1);
-    s2.add(row * COL + COL - 1);
   }
   for (let col = COL - 1; col >= 0; col--) {
     q2.push([ROW - 1, col]);
     v2.add((ROW - 1) * COL + col);
-    s2.add((ROW - 1) * COL + col);
   }
   while (q2.length > 0) {
     const [row, col] = q2.shift();
@@ -87,7 +80,6 @@ var pacificAtlantic = function (heights) {
       }
 
       v2.add(key);
-      s2.add(key);
       q2.push([newRow, newCol]);
     }
   }
@@ -95,8 +87,8 @@ var pacificAtlantic = function (heights) {
   // 收集兩個交集(代表可以同時回到兩個邊界的節點)
   const answers = [];
 
-  for (const key of s1) {
-    if (s2.has(key)) {
+  for (const key of v1) {
+    if (v2.has(key)) {
       const row = Math.floor(key / COL);
       const col = key % COL;
       answers.push([row, col]);
